1914e37df1aeca243444e2a91cbb40cf90fe6f34
Bryan Scalzo <50776347+bryanscalzo97@users.noreply.github.com>
2021-11-17 10:40:34 -0300

    first commit

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <table>
+            <tbody>
+                <tr v-for="todo in todos" :key="todo.id">
+                    <td>{{ todo.id }}</td>
+                    <td>{{ todo.completed }}</td>
+                    <td>{{ todo.title }}</td>
+                    <td>{{ todo.userId }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            todos: null
+            }
+    },
+    mounted(){
+        console.log('Hola mundo desde mounted')
+        this.getTodos();
+    },
+    methods: {
+        getTodos(){
+            console.log('aca va el codigo de get todos')
+            axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then( response => {
+                console.log(response)
+                this.todos = response.data
+            })
+            .catch( e=> console.log(e))
+        },
+        
+        
+        
+    },
+}
+</script>
+<style>
+
+</style>
